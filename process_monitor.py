94ef26bf700d33a6075f7bdb09e8a6f1ec031b3f
BitwiseKatie <yarosh.katie@gmail.com>
2025-03-04 00:36:07 -0500

    Update process_monitor.py

--- process_monitor.py
+++ process_monitor.py
@@ -6,7 +6,7 @@ class ProcessMonitor:
 
     def check_processes(self):
         suspicious_processes = []
-        for proc in psutil.process_iter(['pid', 'name']):
+        for proc in psutil.process_iter(['pid', 'name'])
             try:
                 if any(keyword in proc.info['name'].lower() for keyword in self.suspicious_keywords):
                     suspicious_processes.append(f"Suspicious process: {proc.info['name']} (PID {proc.info['pid']})")
