a2b1b80f5816903d3e4c44f7bc4e137ca74c238a
BitwiseKatie <yarosh.katie@gmail.com>
2025-03-19 21:31:50 -0400

    Update file_monitor.py

--- file_monitor.py
+++ file_monitor.py
@@ -4,23 +4,25 @@ import hashlib
 class FileMonitor:
     def __init__(self, watch_dir="data"):
         self.watch_dir = watch_dir
-        self.files_hashes = self.get_initial_hashes() 
+        self.files_hashes = self.get_initial_hashes()
 
     def get_initial_hashes(self):
         file_hashes = {}
         for root, _, files in os.walk(self.watch_dir):
             for file in files:
                 path = os.path.join(root, file)
-                file_hashes[path] = self.get_file_hash(path)
+                file_hash = self.get_file_hash(path)
+                if file_hash is not None:
+                    file_hashes[path] = file_hash
         return file_hashes
 
     def get_file_hash(self, path):
         hasher = hashlib.md5()
         try:
             with open(path, "rb") as f:
-                buf = f.read()
-                hasher.update(buf)
-        except FileNotFoundError:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hasher.update(chunk)
+        except (FileNotFoundError, PermissionError):
             return None
         return hasher.hexdigest()
 
