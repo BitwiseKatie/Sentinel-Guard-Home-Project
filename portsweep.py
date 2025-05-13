de8c8f6d838567fe89981e1ce5748c9daa1e7645
Katie <yarosh.katie@gmail.com>
2025-05-13 10:17:15 -0400

    Update portsweep.py

--- portsweep.py
+++ portsweep.py
@@ -14,12 +14,26 @@ def sweep_host_ports(
     List[Tuple[int, str]],
     List[Dict[str, Union[str, int, float]]]
 ]:
-    return [
-        result for port in ports
-        if (result := _scan_port(ip, port, timeout, grab_banner, detailed)) is not None
-    ]
+    return list(filter(None, (
+        _scan_port(ip, port, timeout, grab_banner, detailed) for port in ports
+    )))
 
 
-        return (port, banner) if grab_banner else port
-    except:
-        return None
+def _scan_port(
+    ip: str,
+    port: int,
+    timeout: float,
+    grab_banner: bool,
+    detailed: bool
+) -> Optional[Union[int, Tuple[int, str], Dict[str, Union[str, int, float]]]]:
+    try:
+        start = time.perf_counter()
+        with socket.create_connection((ip, port), timeout=timeout) as sock:
+            banner = ""
+            if grab_banner:
+                try:
+                    sock.settimeout(0.3)
+                    banner = sock.recv(1024).decode(errors="ignore").strip() or "N/A"
+                except:
+                    banner = "N/A"
+        elapsed = round((time.perf_counter() - start) * 1000, 2)
