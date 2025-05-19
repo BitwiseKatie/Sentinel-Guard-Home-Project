1ee0d0b3785e20165183806c065bf0f376514a39
Katie <yarosh.katie@gmail.com>
2025-05-19 12:48:50 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -121,5 +121,4 @@ class HomescannerCLI:
         self.db.add_incident(message)
         print(Fore.RED + entry)
 
-    def unknown_command(self):
-        print(Fore.RED + "Unknown command. Type 'help' to list available commands.")
+
