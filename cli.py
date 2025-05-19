e95c4e6cfccccf12b8ba5bff76978fb0359aaae9
Katie <yarosh.katie@gmail.com>
2025-05-19 12:51:33 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -145,5 +145,12 @@ def build_parser():
     return parser
 
 
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
+    cli = HomescannerCLI(args)
+    asyncio.run(cli.run())
+
+
 if __name__ == "__main__":
     main()
