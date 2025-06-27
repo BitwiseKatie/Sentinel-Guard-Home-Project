12f7ea4851bdd9e1f32bd20f417ae976d0cc848c
Katie <yarosh.katie@gmail.com>
2025-06-26 23:28:01 -0400

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -26,11 +26,6 @@ class AlertManager:
             self.logger.addHandler(handler)
         self.logger.setLevel(logging.INFO)
 
-    def send_alert(self, message, severity="warning", source="homescanner"):
-        if not message:
-            self.logger.error("Skipped sending alert: message was empty.")
-            return
-
         entry = f"[{severity.upper()}] {source} - {message}"
         self.logger.warning(entry)
 
